d490f92cac4599bb79c8bb6ef8a0b38dae240396
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2023-10-09 12:00:03 +0200

    Added shebang, spdx and copyright
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

--- twin_api/start_api.sh
+++ twin_api/start_api.sh
@@ -1,3 +1,8 @@
+#!/bin/bash 
+
+# SPDX-FileCopyrightText: 2023, Institute for Automation of Complex Power Systems, EONERC, RWTH Aachen University
+# SPDX-License-Identifier: MPL-2.0
+
 python -m venv venv
 pip install -r requirements-api.txt
 flask --app twin_api.py run -p 31032 -h 0.0.0.0
